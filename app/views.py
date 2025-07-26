3391db5e218d6b448296ca461752a55bd1082ac2
Colin <kappa6708@gmail.com>
2025-07-26 11:25:33 -0500

    fixed routing

--- app/views.py
+++ app/views.py
@@ -8,10 +8,10 @@ bp = Blueprint('main', __name__)
 
 # views
 @bp.route("/")
-def view_index():
+def index():
     return render_template("index.html", title="index")
 
 @bp.route("/puzzle")
-def view_puzzle():
+def puzzle():
     return render_template("puzzle.html", title="puzzle")
 
