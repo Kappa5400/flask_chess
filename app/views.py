2fa92357f06950cb896d9e08bf3872ce7eb7a068
Colin <kappa6708@gmail.com>
2025-07-31 10:38:13 -0500

    frontend update

--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ bp = Blueprint('main', __name__)
 # views
 @bp.route("/")
 def index():
-    return render_template("old_index.html", title="index")
+    return render_template("index.html", title="index")
 
 @bp.route("/puzzle/<int:puzzle_id>")
 def puzzle(puzzle_id):
@@ -20,3 +20,29 @@ def puzzle(puzzle_id):
     parsed_date = datetime.strptime(puzzle['date'], '%Y-%m-%d')
     return render_template("puzzle.html", title="puzzle", puzzle=puzzle, date=parsed_date)
 
+@bp.route("/daily")
+def daily_puzzle():
+    db = get_db()
+    puzzle = db.execute("select * from puzzle order by date desc limit 1 ").fetchone()
+    #passing date type to template
+    parsed_date = datetime.strptime(puzzle['date'], '%Y-%m-%d')
+    return render_template("puzzle.html", title="Daily Puzzle",
+                           puzzle=puzzle, date=parsed_date)
+
+@bp.route('/puzzle/<int:offset>')
+def puzzle_offset(offset):
+    offset = int(offset)
+    if offset >= 10 or offset < 0:
+        return jsonify({"Error": "Offset our of range"}), 404
+
+    db = get_db()
+    puzzle = db.execute(
+        "SELECT * FROM puzzle ORDER BY date ASC LIMIT 1 OFFSET ?",
+        (offset,)
+    ).fetchone()
+    date = datetime.strptime(puzzle['date'],'%Y-%m-%d')
+
+    if puzzle is None:
+        return jsonify({"Error": "No puzzle found"}), 404
+
+    return render_template("puzzle.html", title="puzzle", puzzle=puzzle, date=date)
